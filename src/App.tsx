b73117a3cbf0d6dd027d856fd9978297aa60cfd3
Joshua Flanagan <josh_flanagan@live.co.uk>
2022-01-21 23:45:39 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Example project containing all Josh Ideas</p>
+        <h3>Technologies include:</h3>
+        <ul>
+          <li>React</li>
+          <li>Typescript</li>
+          <li>Storybook</li>
+          <li>Vite</li>
+          <li>Tailwind (todo)</li>
+          <li>Jest (todo)</li>
+          <li>Cypress (todo)</li>
+          <li>Apollo Client Typegen (todo)</li>
+          <li>SSR (todo)</li>
+        </ul>
+      </header>
+    </div>
+  )
+}
+
+export default App
