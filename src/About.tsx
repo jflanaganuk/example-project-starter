283f60ed712b811bf4ea4d0a8973516bf00b89a5
Joshua Flanagan <josh_flanagan@live.co.uk>
2024-04-19 11:08:52 +0300

    Added example fetch to project

--- src/About.tsx
+++ src/About.tsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+
+// This is a simple example of a component that fetches data from an API.
+const API = "https://pokeapi.co/api/v2/ability/?limit=20&offset=20";
+
 export default function About() {
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    fetch(API, {
+      signal,
+    })
+      .then((response): Promise<any> => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return <h2>About</h2>;
 }
